2f145ace9ffd6591ca9987f0cb6cd489aa6b1ab3
user <shlomoshlomoshlomo@gmail.com>
2017-03-05 17:59:21 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from scapy.all import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_app():
+	return render_template("index.html")
+
+@app.route("/post_request", methods=["POST"])
+def post_request():
+	return request.form["test1"]
+
+if __name__ == "__main__":
+	# app.run()
+	app.run(host="0.0.0.0", port=8000)
