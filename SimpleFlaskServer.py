c329b57113538780541f1dddc4ac1b3961fdf8b4
user <shlomoshlomoshlomo@gmail.com>
2017-03-08 17:00:32 +0200

    added some more content

--- SimpleFlaskServer.py
+++ SimpleFlaskServer.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 # from scapy.all import *
 
 app = Flask(__name__)
@@ -15,6 +16,9 @@ def main_app():
 def post_request():
 	return request.form["test1"]
 
+@app.route("/reroute")
+def reroute():
+	return redirect("/static/9.html", code=302)
 
 if __name__ == "__main__":
 	app.run()
